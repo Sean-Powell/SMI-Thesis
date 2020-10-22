83686707d1003045d87aeda8d176e87bf0e7fc63
Sean <seanpowell54@gmail.com>
2020-10-22 20:22:42 +0200

    Added the cross over function and the main GA loop

--- Expression.py
+++ Expression.py
@@ -8,13 +8,19 @@ class Expression:
     terms = []
     max_exponent = 0
     max_coefficient = 0
+    mutation_chance = 10
+    chi_2 = 0
 
-    def __init__(self, functions, grammar, min_size=1, max_size=5, max_exponent=3, max_coefficient=9):
+    def __init__(self, functions, grammar, min_size=1, max_size=5, max_exponent=3, max_coefficient=9,
+                 mutation_chance=10):
         self.functions = functions
         self.grammar = grammar
         self.terms = []
         self.max_exponent = max_exponent
         self.max_coefficient = max_coefficient
+        self.mutation_chance = mutation_chance
+        self.chi_2 = 0
+
         size = randint(min_size, max_size)
         for _ in range(size):
             sign = randint(0, 1)
@@ -38,14 +44,49 @@ class Expression:
         print(output)
 
     def mutation(self):
-        # 50% chance for each term to mutate
-        for term in self.terms:
+        # 10% chance for a random term to be removed (iff length of terms is more than 1) or added to the expression
+        chance = randint(0, 100)
+        if chance <= self.mutation_chance:
+            chance = randint(0, 1)
+            if chance == 0 and len(self.terms) > 1:
+                index = randint(0, (len(self.terms) - 1))
+                print("removing", index)
+                self.terms.remove(self.terms[index])
+            elif chance == 1:
+                print("Adding new term")
+                sign = randint(0, 1)
+                operation = self.grammar[randint(0, len(self.grammar) - 1)]
+                exponent = randint(1, self.max_exponent)
+                coefficient = randint(1, self.max_coefficient)
+                self.terms.append(Term(sign, operation, exponent, coefficient))
+
+        else:
+            # for term in self.terms:
+            index = randint(0, (len(self.terms) - 1))
+            print("modifying index", index)
+            term = self.terms[index]
+
             chance = randint(0, 1)
             if chance == 0:
-                term.set_coefficient(randint(1, self.max_coefficient))
+                term.set_coefficient(term.coefficient - 1)
+            elif chance == 1:
+                term.set_coefficient(term.coefficient + 1)
+
             chance = randint(0, 1)
             if chance == 0:
-                term.set_exponent(randint(1, self.max_exponent))
+                term.set_exponent(term.exponent - 1)
+            elif chance == 1:
+                term.set_exponent(term.exponent + 1)
+
             chance = randint(0, 1)
             if chance == 0:
                 term.flip_sign()
+
+    def set_terms(self, terms):
+        self.terms = terms
+
+    def get_chi_2(self):
+        return self.chi_2
+
+    def set_chi_2(self, chi_2):
+        self.chi_2 = chi_2
--- GA.py
+++ GA.py
@@ -1,22 +1,102 @@
 from Expression import Expression
-from math import sin, cos, tan, log
+from math import sin, cos, tan, log, pow, ceil
+from random import randint
 
 
 POPULATION_SIZE = 500
+MUTATION_CHANCE = 5
+NUMBER_OF_GENERATIONS = 100
+SELECTION_RATE = 10
 
 functions = {"sin": sin, "cos": cos, "tan": tan, "ln": log}
 grammar = ['sin()', 'cos()', 'tan()', 'ln()', '']
 
-population = []
-for i in range(POPULATION_SIZE):
-    population.append(Expression(functions, grammar))
+y = []
+x = []
+s = []
 
-print("Created population")
 
-for i in population:
-    i.print()
-    i.mutation()
-    i.print()
-    print("-----")
+def fitness_function(func):
+    n = len(x) - 1  # todo check if this -1 is needed when given the data set
+    chi2 = 0
+    for i in range(1, n):
+        chi2 += pow(((y[i] - func.evaluate(x[i])) / s[i]), 2)
+    return chi2
 
-# todo implement reproduction systems
+
+def get_terms(index, candidate_1: Expression, candidate_2: Expression):
+    terms = []
+    for _ in range(index):
+        chance = randint(0, 1)
+        if chance == 0:
+            term = candidate_1.terms[randint(0, (len(candidate_1.terms) - 1))]
+            chance = randint(0, 1)
+            if chance == 1:
+                term.exponent = candidate_2.terms[randint(0, len(candidate_2.terms) - 1)].exponent
+            chance = randint(0, 1)
+            if chance == 1:
+                term.coefficient = candidate_2.terms[randint(0, len(candidate_2.terms) - 1)].coefficient
+        else:
+            term = candidate_2.terms[randint(0, (len(candidate_2.terms) - 1))]
+            chance = randint(0, 1)
+            if chance == 1:
+                term.exponent = candidate_1.terms[randint(0, len(candidate_1.terms) - 1)].exponent
+            chance = randint(0, 1)
+            if chance == 1:
+                term.coefficient = candidate_1.terms[randint(0, len(candidate_1.terms) - 1)].coefficient
+        terms.append(term)
+    return terms
+
+
+def crossover(candidate_1: Expression, candidate_2: Expression):
+    n = len(candidate_1.terms)
+    j = len(candidate_2.terms)
+    k = ceil((n + j) / 2)
+
+    child_1 = Expression(functions, grammar, mutation_chance=MUTATION_CHANCE)
+    child_1.set_terms(get_terms(n, candidate_1, candidate_2))
+    child_2 = Expression(functions, grammar, mutation_chance=MUTATION_CHANCE)
+    child_2.set_terms(get_terms(j, candidate_1, candidate_2))
+    child_3 = Expression(functions, grammar, mutation_chance=MUTATION_CHANCE)
+    child_3.set_terms(get_terms(k, candidate_1, candidate_2))
+
+    return child_1, child_2, child_3
+
+
+def start():
+    population = []
+    for p in range(POPULATION_SIZE):
+        population.append(Expression(functions, grammar, mutation_chance=MUTATION_CHANCE))
+        population[p].set_chi_2(randint(0, POPULATION_SIZE))
+
+    for i in range(NUMBER_OF_GENERATIONS):
+        for j in population:
+            j.set_chi_2(fitness_function(j))
+
+        #  todo evaluate the chi_2 on  each of the population
+        population.sort(key=lambda l: l.get_chi_2(), reverse=False)
+
+        print("sorted population")
+
+        select_amount = POPULATION_SIZE // SELECTION_RATE
+        if select_amount % 2 != 0:
+            select_amount += 1
+
+        parents = []
+        new_generation = []
+        for j in range(select_amount):
+            parents.append(population[j])
+
+        for j in range((select_amount - 1)):
+            a, b, c = crossover(parents[j], parents[j + 1])
+            new_generation.append(a)
+            new_generation.append(b)
+            new_generation.append(c)
+
+        for j in new_generation:
+            chance = randint(0, 100)
+            if chance < MUTATION_CHANCE:
+                j.mutation()
+
+
+start()
